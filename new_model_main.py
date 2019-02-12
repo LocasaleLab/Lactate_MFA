d844e6be04d4a1ac35ebc2ae6315824593616f91
Shiyu (Baran) Liu <liushiyu1994@gmail.com>
2019-02-12 02:22:25 -0500

    Change parameters

--- new_model_main.py
+++ new_model_main.py
@@ -605,7 +605,7 @@ def dynamic_range_model4(model_mid_data_dict: dict, total_output_direct, paralle
         ternary_resolution = int(2 ** 7)
     else:
         total_point_num = int(3e6)
-        point_interval_list = [50, 50, 10, 10, 50]
+        point_interval_list = [25, 25, 5, 5, 25]
         ternary_resolution = int(2 ** 8)
 
     free_fluxes_list = ['F1', 'G2', 'F9', 'G10', 'F3']
