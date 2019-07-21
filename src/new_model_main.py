8952d83ecbd2a7c1cb55c3ba06bfdfdd7f2e742b
Shiyu (Baran) Liu <liushiyu1994@gmail.com>
2019-07-21 17:25:20 -0400

    Check

--- src/new_model_main.py
+++ src/new_model_main.py
@@ -672,28 +672,28 @@ def linear_main():
 
 
 def non_linear_main():
-    model_parameter_dict = model_specific_functions.model1_parameters()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model1_parameters()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
     # model_parameter_dict = model_specific_functions.model2_parameters()
     # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model3_parameters()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model5_parameters()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model6_parameters()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model7_parameters()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model1_all_tissue()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model1_parameter_sensitivity()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model1_m5_parameters()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model3_parameters()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model5_parameters()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model6_parameters()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model7_parameters()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model1_all_tissue()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model1_parameter_sensitivity()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model1_m5_parameters()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
     model_parameter_dict = model_specific_functions.model1_m9_parameters()
     parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
-    model_parameter_dict = model_specific_functions.model3_all_tissue()
-    parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
+    # model_parameter_dict = model_specific_functions.model3_all_tissue()
+    # parallel_solver(**model_parameter_dict, one_case_solver_func=one_case_solver_slsqp)
     # fitting_result_display(**model_parameter_dict)
 
 
@@ -721,4 +721,5 @@ def parser_main():
 
 
 if __name__ == '__main__':
-    parser_main()
+    non_linear_main()
+    # parser_main()
